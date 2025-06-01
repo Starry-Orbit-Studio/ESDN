908c286c17ea0779c8e49c08b94a334213efe7bf
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-06-01 12:24:39 +0800

    使单位文档生成扩展能够正常工作
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ plugins/unitdoc/client/components/UnitTypeCategory.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-for="(type, index) in data" :key="index">
+    <RouterLink :to="`./${type}/`">
+      {{ i18n('typeName', type + 'Types', locale.lang) }}
+    </RouterLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useSiteLocaleData } from '@vuepress/client'
+import { RouterLink } from 'vue-router'
+import common from '../common'
+const data = Object.keys(__ESDNUnitDoc.indexes)
+const locale = useSiteLocaleData()
+const { i18n } = common()
+</script>
